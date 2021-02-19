72aa252308f0aaf0eafd900ac39dac9e900ac9ef
Muhammad K <akhalid.code@gmail.com>
2021-02-18 22:03:00 -0800

    add workout and exercise models

---
+++ models/exercise.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+	type: {
+		type: String,
+		enum: ["resistance", "cardio"]
+	},
+	name: {
+		type: String,
+		required: "Enter a name you weak fool"
+	},
+	duration: {
+		type: Number,
+		required: "How long did you exercise. Don't lie",
+		min: 1
+	},
+	weight: {
+		type: Number,
+		min: 1
+	},
+	reps: {
+		type: Number,
+		min: 1
+	},
+	sets: {
+		type: Number,
+		min: 1
+	},
+	distance: {
+		type: Number,
+		min: 1
+	}
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
---
+++ models/workout.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+	date: {
+		type: Date,
+		required: "Enter a date"
+	},
+	exercises: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Exercises"
+		}
+	]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
